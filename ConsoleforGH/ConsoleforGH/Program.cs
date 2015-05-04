5ddd508c805606fbf76f3e90d421177960d6eced
IAmYourBrother <josephrhodiumsharp@gmail.com>
2015-05-04 16:31:26 +0800

    initCommit

---
+++ ConsoleforGH/ConsoleforGH/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleforGH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello varld");
+        }
+    }
+}
